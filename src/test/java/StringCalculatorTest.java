6636cb5cf9f2b1a9da13fb577c94dc540ffdb7c9
JakSmig <248908@student.pwr.edu.pl>
2022-06-13 19:31:04 +0200

    Step 2

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -12,5 +12,8 @@ class StringCalculatorTest {
         assertEquals(0, calc.Add(""));
         assertEquals(1, calc.Add("1"));
         assertEquals(3, calc.Add("1,2"));
+
+        //Step 2
+        assertEquals(15, calc.Add("1,2,3,4,5"));
     }
 }
